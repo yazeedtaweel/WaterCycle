8984f6dad0626dbdf63482f7ff58b9ed4ae6ffb9
latifaIssa <latifa.masri1998@gmail.com>
2023-02-26 19:34:06 +0200

    push_notification

---
+++ lib/services/register_notification.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../models/push_notification_model.dart';
+
+class RegisterNotification extends StatefulWidget {
+  const RegisterNotification({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterNotification> createState() => _RegisterNotificationState();
+}
+
+class _RegisterNotificationState extends State<RegisterNotification> {
+  late final FirebaseMessaging _messaging;
+  PushNotification? _notificationInfo;
+
+  void registerNotification() async {
+    // 2. Instantiate Firebase Messaging
+    _messaging = FirebaseMessaging.instance;
+
+    // 3. On iOS, this helps to take the user permissions
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+
+      // For handling the received notifications
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          // _totalNotifications++;
+        });
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
+      });
+
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notify'),
+        brightness: Brightness.dark,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          _notificationInfo != null
+              ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'TITLE: ${_notificationInfo!.title}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'BODY: ${_notificationInfo!.body}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
+
+
